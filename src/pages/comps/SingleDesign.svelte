c0506f9eb68197a38447421cba59998613338e3b
enix403 <fog.code000@gmail.com>
2024-12-10 23:49:26 +0500

    Pick Designs

--- src/pages/comps/SingleDesign.svelte
+++ src/pages/comps/SingleDesign.svelte
@@ -5,23 +5,39 @@
   export let selected: boolean = false;
 </script>
 
-<button
-  class={clsx("overflow-hidden relative rounded-lg border-4", selected && "border-pink-500")}
->
-  <img
-    class="h-auto max-w-full"
-    src="/plan1.jpg"
-    alt=""
-  />
+<button class="relative group cursor-pointer">
+  <div
+    class={clsx(
+      "absolute -inset-1 bg-gradient-to-r from-red-600 to-violet-600 rounded-lg blur",
+      selected ? "opacity-100" : "opacity-25"
+    )}
+  ></div>
+  <div
+    class={clsx(
+      "relative bg-white rounded-2xl border-4 overflow-hidden",
+      selected ? "border-pink-400" : ""
+    )}
+  >
+    <div class="relative bg-black">
+      <img class="h-auto max-w-full" src="/plan1.jpg" alt="" />
 
-  <div class="absolute inset-0">
-    <div class="w-full h-full p-2 flex flex-col items-end">
-      <button class="btn btn-circle bg-black/40">
-        <IconEye />
-      </button>
+      <div class="absolute inset-0">
+        <div class="w-full h-full p-2 flex flex-col items-end">
+          <button class="btn btn-circle bg-black/40">
+            <IconEye />
+          </button>
+        </div>
+      </div>
     </div>
   </div>
 </button>
+
+<!--
+
+
+
+
+ -->
 
 <!--
 

2c59b401c8ef97b9968ec0736062cfa7a9435944
enix403 <fog.code000@gmail.com>
2024-12-10 23:20:49 +0500

    TOpbar

---
+++ src/pages/comps/AppBar.svelte
@@ -0,0 +1,17 @@
+<nav class="border-0 border-b-2 border-border">
+  <div class="flex flex-wrap items-center justify-between p-4">
+    <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
+      <!-- <img
+        src="https://FrameCraft.com/docs/images/logo.svg"
+        class="h-8"
+        alt="FrameCraft Logo"
+      /> -->
+      <span class="self-center text-2xl font-semibold whitespace-nowrap">
+        FrameCraft
+      </span>
+    </a>
+    <div class="avatar avatar-ring-secondary">
+      <img src="https://i.pravatar.cc/150?img=57" alt="avatar" />
+    </div>
+  </div>
+</nav>

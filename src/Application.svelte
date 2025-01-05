8e07d5ce0008afc32cc550b5c8c291d2c256678f
enix403 <fog.code000@gmail.com>
2025-01-05 17:27:23 +0500

    Tweaks

--- src/Application.svelte
+++ src/Application.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
   import { Router, Route } from "svelte-routing";
   import Home from "./pages/home/Home.svelte";
-  import Login from "./pages/Login.svelte";
-  import Register from "./pages/Register.svelte";
+  import Login from "./pages/auth/Login.svelte";
+  import Register from "./pages/auth/Register.svelte";
   import Generate from "./pages/generate/Generate.svelte";
-  import FinalizeDesign from "./pages/generate/FinalizeDesign.svelte";
+  import EdiDesign from "./pages/generate/EditDesign.svelte";
   import ViewRender from "./pages/generate/ViewRender.svelte";
   import MyDesigns from "./pages/generate/MyDesigns.svelte";
 </script>
@@ -13,8 +13,8 @@
   <Route path="/" component={Home} />
   <Route path="/login" component={Login} />
   <Route path="/register" component={Register} />
-  <Route path="/gen" component={Generate} />
-  <Route path="/final" component={FinalizeDesign} />
+  <Route path="/generate" component={Generate} />
+  <Route path="/edit" component={EdiDesign} />
   <Route path="/render" component={ViewRender} />
-  <Route path="/my" component={MyDesigns} />
+  <Route path="/my-designs" component={MyDesigns} />
 </Router>
--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [tsconfigPaths(), svelte()],
 
   server: {
-    port: 4200,
+    port: 3000,
     host: '0.0.0.0'
   }
 });
